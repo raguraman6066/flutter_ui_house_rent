25b98d664fbc354dea5b46c5fa5e3d548bb654a0
ragu raman <raguraman@gmail.com>
2024-04-21 18:41:48 -0700

    initial commit

---
+++ android/app/src/main/kotlin/com/mpsragu/flutter_house_rent_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mpsragu.flutter_house_rent_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen/home/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'widget/best_offer.dart';
+import 'widget/categories.dart';
+import 'widget/custom_app_bar.dart';
+import 'widget/custom_bottom_navigation_bar.dart';
+import 'widget/recommended_house.dart';
+import 'widget/search_input.dart';
+import 'widget/welcome_text.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: CustomAppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            WelcomeText(),
+            SearchInput(),
+            Categories(),
+            RecommendedHouse(),
+            BestOffer()
+          ],
+        ),
+      ),
+      bottomNavigationBar: CustomBottomNavigationBar(),
+    );
+  }
+}
